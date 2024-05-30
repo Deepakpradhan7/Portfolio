c0722298c9505f4f77d033061ceff0ef07a92854
Deepupradhan <118422944+Deepupradhan@users.noreply.github.com>
2024-05-30 11:31:43 +0530

    Flex gap

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = () => {
         />
       </div>
 
-      <div className="flex justify-center relative my-20 z-10">
+      <div className="flex justify-center relative md:my-14 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Hello, tech world!
